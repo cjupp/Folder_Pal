fe88ab00fb5539d22ec2d9e0c6995aac06702ac0
Cameron Jupp <cameronjupp@hotmail.com>
2023-08-07 21:56:25 -0700

    Added notes

--- File_Pal_GUI.py
+++ File_Pal_GUI.py
@@ -1,6 +1,20 @@
 import tkinter as tk
 import File_Pal_Module as fp
 
+#-------------------------------------------------------------------------------------------------------------------- |
+#--------------------------------  / \  / /  / __  /  /__ __/  / ___/  / ____/ --------------------------------------- |
+#-------------------------------  / /\\/ /  / /_/ /    / /    / __/    \__ \  ---------------------------------------- |
+#------------------------------  /_/  \_/  /_____/    /_/    /____/  /_____/ ----------------------------------------- |
+#--------------------------------------------------------------------------------------------------------------------- |
+# Last left off at:
+#
+# - Scrollable listbox to hold subfolders
+# - Choose folder path after clicking on browse button
+# - Subfolders input text box, with button that adds string into new line in listbox
+# - Spinbox to select or input number of folders
+#
+#--------------------------------------------------------------------------------------------------------------------- */
+
 path = 0
 prefix_string = 0
 suffix_string = 0
@@ -8,5 +22,7 @@ folder_name = 0
 
 folder_count = 0
 subfolders = 0
+
+indexing_types = "Numerical", "Alphabetical"
 
 
